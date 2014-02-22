1c59f703518da5fc58a01955271e5f8f78649d26
Alexander Fabisch <afabisch@googlemail.com>
2014-02-22 01:24:41 +0100

    Add functions

--- gmm.py
+++ gmm.py
@@ -4,3 +4,28 @@ from multivariate_normal import MultivariateNormal
 class GMM(object):
     def __init__(self):
         pass
+
+    def from_samples(self, X):
+        """EM."""
+        pass
+        # TODO
+
+    def sample(self, n_samples):
+        pass
+        # TODO
+
+    def to_probability_density(self, X):
+        pass
+        # TODO
+
+    def marginalize(self, indices):
+        pass
+        # TODO
+
+    def condition(self, indices, x):
+        pass
+        # TODO
+
+    def predict(self, indices, x):
+        pass
+        # TODO
