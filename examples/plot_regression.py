f43412bce31dbe9d516c475bcc417e1b7dd35e3c
Alexander Fabisch <afabisch@googlemail.com>
2014-02-23 22:35:11 +0100

    Clean up regression example

--- examples/plot_regression.py
+++ examples/plot_regression.py
@@ -15,25 +15,33 @@ if __name__ == "__main__":
     mvn = MVN(random_state=0)
     mvn.from_samples(X)
 
-    plt.scatter(X[:, 0], X[:, 1])
     X_test = np.linspace(0, 2 * np.pi, 100)
     mean, covariance = mvn.predict(np.array([0]), X_test[:, np.newaxis])
+
+    plt.figure(figsize=(10, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.title("Linear: $p(Y | X) = \mathcal{N}(\mu_{Y|X}, \Sigma_{Y|X})$")
+    plt.scatter(X[:, 0], X[:, 1])
     y = mean.ravel()
     s = covariance.ravel()
-    plt.plot(X_test, y)
     plt.fill_between(X_test, y - s, y + s, alpha=0.2)
+    plt.plot(X_test, y, lw=2)
 
     n_samples = 100
     X = np.ndarray((n_samples, 2))
     X[:, 0] = np.linspace(0, 2 * np.pi, n_samples)
     X[:, 1] = np.sin(X[:, 0]) + random_state.randn(n_samples) * 0.1
 
-    plt.figure()
     gmm = GMM(n_components=3, random_state=0)
     gmm.from_samples(X)
     Y = gmm.predict(np.array([0]), X_test[:, np.newaxis])
-    plt.plot(X_test, Y.ravel())
+
+    plt.subplot(1, 2, 2)
+    plt.title("Mixture of Experts: $p(Y | X) = \Sigma_k \pi_{k, Y|X} "
+              "\mathcal{N}_{k, Y|X}$")
     plt.scatter(X[:, 0], X[:, 1])
-    plot_error_ellipses(plt.gca(), gmm, colors=["r", "g", "b", "y"])
+    plot_error_ellipses(plt.gca(), gmm, colors=["r", "g", "b"])
+    plt.plot(X_test, Y.ravel(), c="k", lw=2)
 
     plt.show()
