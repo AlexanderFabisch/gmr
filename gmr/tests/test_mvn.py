ac44d9d1b12975f65930bf1be4b7cd6e674b450b
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2014-08-23 16:33:56 +0200

    Add test for MVN

--- gmr/tests/test_mvn.py
+++ gmr/tests/test_mvn.py
@@ -1,13 +1,26 @@
 import numpy as np
 from sklearn.utils import check_random_state
 from nose.tools import assert_equal, assert_less
-from gmr import MVN
+from gmr import MVN, plot_error_ellipse
+
+
+mean = np.array([0.0, 1.0])
+covariance = np.array([[0.5, -1.0], [-1.0, 5.0]])
+
+
+class AxisStub:
+    def __init__(self):
+        self.count = 0
+        self.artists = []
+
+    def add_artist(self, artist):
+        self.artists.append(artist)
+        self.count += 1
 
 
 def test_estimate_moments():
     """Test moments estimated from samples and sampling from MVN."""
     random_state = check_random_state(0)
-
     actual_mean = np.array([0.0, 1.0])
     actual_covariance = np.array([[0.5, -1.0], [-1.0, 5.0]])
     X = random_state.multivariate_normal(actual_mean, actual_covariance,
@@ -28,9 +41,6 @@ def test_estimate_moments():
 def test_probability_density():
     """Test PDF of MVN."""
     random_state = check_random_state(0)
-
-    mean = np.array([0.0, 1.0])
-    covariance = np.array([[0.5, -1.0], [-1.0, 5.0]])
     mvn = MVN(mean, covariance, random_state=random_state)
 
     x = np.linspace(-100, 100, 201)
@@ -43,9 +53,6 @@ def test_probability_density():
 def test_marginal_distribution():
     """Test moments from marginal MVN."""
     random_state = check_random_state(0)
-
-    mean = np.array([0.0, 1.0])
-    covariance = np.array([[0.5, -1.0], [-1.0, 5.0]])
     mvn = MVN(mean=mean, covariance=covariance, random_state=random_state)
 
     marginalized = mvn.marginalize(np.array([0]))
@@ -84,3 +91,13 @@ def test_ellipse():
     assert_equal(angle, 0.5 * np.pi)
     assert_equal(width, np.sqrt(5.0))
     assert_equal(height, np.sqrt(0.5))
+
+
+def test_plot():
+    """Test plot of MVN."""
+    random_state = check_random_state(0)
+    mvn = MVN(mean=mean, covariance=covariance, random_state=random_state)
+
+    ax = AxisStub()
+    plot_error_ellipse(ax, mvn)
+    assert_equal(ax.count, 8)
