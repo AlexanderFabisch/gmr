9c503bd0aaa64793a92225f3f562de6b6703ee2d
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2014-08-28 10:01:17 +0200

    Add dependency scipy

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def setup_package():
             "Programming Language :: Python :: 3.3",
         ],
         packages=["gmr"],
-        requires=["numpy", "sklearn"],
+        requires=["numpy", "scipy", "sklearn"],
     )
 
 
