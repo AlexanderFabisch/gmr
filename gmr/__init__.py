02380c10c0aa9b52afdde867d53d5368d355ef5c
Alexander Fabisch <alexander.fabisch@dfki.de>
2018-08-08 10:58:29 +0200

    Import submodules before export

--- gmr/__init__.py
+++ gmr/__init__.py
@@ -7,6 +7,8 @@ Gaussian Mixture Models (GMMs) for clustering and regression in Python.
 
 __version__ = "1.1"
 
+from . import gmm, mvn, utils
+
 __all__ = ['gmm', 'mvn', 'utils']
 
 from .mvn import MVN, plot_error_ellipse
