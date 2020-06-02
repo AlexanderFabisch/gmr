a052333cd4c31e830e9155da222a12da08474c62
Alexander Fabisch <afabisch@googlemail.com>
2020-06-02 23:15:23 +0200

    Better illustration

--- examples/plot_trajectories.py
+++ examples/plot_trajectories.py
@@ -1,4 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+from itertools import cycle
+from gmr import GMM, plot_error_ellipses, kmeansplusplus_initialization, covariance_initialization, plot_error_ellipse
+from gmr.utils import check_random_state
 
 
 def make_demonstrations(n_demonstrations, n_steps, sigma=0.25, mu=0.5,
@@ -74,62 +79,74 @@ def make_demonstrations(n_demonstrations, n_steps, sigma=0.25, mu=0.5,
     return X, ground_truth
 
 
-if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-    from gmr import GMM, plot_error_ellipses, kmeansplusplus_initialization, covariance_initialization, plot_error_ellipses
-    from gmr.utils import check_random_state
+plot_covariances = False
+X, _ = make_demonstrations(
+    n_demonstrations=500, n_steps=20, goal=np.array([1., 2.]),
+    random_state=0)
+X = X.transpose(2, 1, 0)
+n_demonstrations, n_steps, n_task_dims = X.shape
+X_train = np.empty((n_demonstrations, n_steps, n_task_dims + 1))
+X_train[:, :, 1:] = X
+t = np.linspace(0, 1, n_steps)
+X_train[:, :, 0] = t
+X_train = X_train.reshape(n_demonstrations * n_steps, n_task_dims + 1)
 
-    X, _ = make_demonstrations(
-        n_demonstrations=200, n_steps=100, goal=np.array([1., 2.]),
-        random_state=0)
-    X = X.transpose(2, 1, 0)
-    n_demonstrations, n_steps, n_task_dims = X.shape
-    X_train = np.empty((n_demonstrations, n_steps, n_task_dims + 1))
-    X_train[:, :, 1:] = X
-    t = np.linspace(0, 1, n_steps)
-    X_train[:, :, 0] = t
-    X_train = X_train.reshape(n_demonstrations * n_steps, n_task_dims + 1)
+random_state = check_random_state(0)
+n_components = 10
+initial_means = kmeansplusplus_initialization(X_train, n_components, random_state)
+initial_covs = covariance_initialization(X_train, n_components)
+from sklearn.mixture import BayesianGaussianMixture
+bgmm = BayesianGaussianMixture(n_components=n_components, max_iter=500).fit(X_train)
+gmm = GMM(
+    n_components=n_components,
+    priors=bgmm.weights_,
+    means=bgmm.means_,
+    #means=np.copy(initial_means),
+    covariances=bgmm.covariances_,
+    #covariances=initial_covs,
+    verbose=2,
+    random_state=random_state)
+#gmm.from_samples(
+#    X_train, n_iter=100,
+#    reinit_means=True, min_eff_sample=n_task_dims, max_eff_sample=0.8)
 
-    random_state = check_random_state(0)
-    n_components = 5
-    initial_means = kmeansplusplus_initialization(X_train, n_components, random_state)
-    initial_covs = covariance_initialization(X_train, n_components)
-    gmm = GMM(n_components=n_components,
-            priors=np.ones(n_components, dtype=np.float) / n_components,
-            means=np.copy(initial_means),
-            covariances=initial_covs,
-            verbose=2,
-            random_state=random_state)
-    gmm.from_samples(X_train, n_iter=200, max_eff_sample=0.5)
+plt.figure()
+plt.subplot(111)
 
-    plt.figure()
-    plt.subplot(111)
+plt.plot(X[:, :, 0].T, X[:, :, 1].T, c="k", alpha=0.1)
 
-    for step in t:
-        conditional_gmm = gmm.condition([0], np.array([step]))
-        samples = conditional_gmm.sample(100)
-        #plot_error_ellipses(plt.gca(), conditional_gmm, colors=["r", "g", "b"])
-        #print(conditional_gmm.priors)
-        #print(conditional_gmm.means)
-        #print(conditional_gmm.covariances)
-        plt.scatter(samples[:, 0], samples[:, 1], s=10)
+means_over_time = []
+y_stds = []
+for step in t:
+    conditional_gmm = gmm.condition([0], np.array([step]))
+    conditional_mvn = conditional_gmm.to_mvn()
+    means_over_time.append(conditional_mvn.mean)
+    y_stds.append(conditional_mvn.covariance[1, 1])
+    samples = conditional_gmm.sample(100)
+    plt.scatter(samples[:, 0], samples[:, 1], s=1)
+means_over_time = np.array(means_over_time)
+y_stds = np.array(y_stds)
+plt.plot(means_over_time[:, 0], means_over_time[:, 1], c="r", lw=2)
+plt.fill_between(
+    means_over_time[:, 0],
+    means_over_time[:, 1] - 1.96 * y_stds,
+    means_over_time[:, 1] + 1.96 * y_stds,
+    color="r", alpha=0.5)
 
-    from matplotlib.patches import Ellipse
-    from itertools import cycle
+if plot_covariances:
     colors = cycle(["r", "g", "b"])
     for factor in np.linspace(0.5, 4.0, 8):
-        new_gmm = GMM(n_components=len(gmm.means), priors=gmm.priors, means=gmm.means[:, 1:], covariances=gmm.covariances[:, 1:, 1:], random_state=gmm.random_state)
-        #k = 0
+        new_gmm = GMM(
+            n_components=len(gmm.means), priors=gmm.priors,
+            means=gmm.means[:, 1:], covariances=gmm.covariances[:, 1:, 1:],
+            random_state=gmm.random_state)
         for mean, (angle, width, height) in new_gmm.to_ellipses(factor):
             ell = Ellipse(xy=mean, width=width, height=height,
-                          angle=np.degrees(angle))
+                            angle=np.degrees(angle))
             ell.set_alpha(0.2)
-            #ell.set_alpha(new_gmm.priors[k])
-            #k += 1
             ell.set_color(next(colors))
             plt.gca().add_artist(ell)
 
-    plt.plot(X[:, :, 0].T, X[:, :, 1].T, alpha=0.2)
-    plt.xlabel("$x_1$")
-    plt.ylabel("$x_2$")
-    plt.show()
+plt.xlabel("$x_1$")
+plt.ylabel("$x_2$")
+plt.show()
