a85e076e37b480515cf2b8ffc1240f5a42fb8b50
Alexander Fabisch <afabisch@googlemail.com>
2021-03-06 20:53:23 +0100

    Add API doc

--- gmr/__init__.py
+++ gmr/__init__.py
@@ -9,8 +9,11 @@ __version__ = "1.5.dev"
 
 from . import gmm, mvn, utils
 
-__all__ = ['gmm', 'mvn', 'utils']
+__all__ = ["gmm", "mvn", "utils"]
 
 from .mvn import MVN, plot_error_ellipse
 from .gmm import (GMM, plot_error_ellipses, kmeansplusplus_initialization,
                   covariance_initialization)
+
+__all__.extend(["MVN", "plot_error_ellipse", "GMM", "plot_error_ellipses",
+                "kmeansplusplus_initialization", "covariance_initialization"])
