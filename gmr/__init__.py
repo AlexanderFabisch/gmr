8ad5542cb7f1df11ea8d3fb73c3a5a22a3e574e8
Alexander Fabisch <afabisch@googlemail.com>
2025-08-13 12:32:03 +0200

    Version 2.0.1: pyproject.toml

--- gmr/__init__.py
+++ gmr/__init__.py
@@ -5,26 +5,16 @@ gmr
 Gaussian Mixture Models (GMMs) for clustering and regression in Python.
 """
 
-__version__ = "2.0.0"
-
-try:
-    # Idea from sklearn:
-    # This variable is injected in the __builtins__ by the build
-    # process. It is used to enable importing subpackages when
-    # the dependencies are not available.
-    __GMR_SETUP__
-except NameError:
-    __GMR_SETUP__ = False
+__version__ = "2.0.1"
 
 
-if not __GMR_SETUP__:
-    from . import gmm, mvn, utils
+from . import gmm, mvn, utils
 
-    __all__ = ["gmm", "mvn", "utils", "sklearn"]
+__all__ = ["gmm", "mvn", "utils", "sklearn"]
 
-    from .mvn import MVN, plot_error_ellipse
-    from .gmm import (GMM, plot_error_ellipses, kmeansplusplus_initialization,
-                      covariance_initialization)
+from .mvn import MVN, plot_error_ellipse
+from .gmm import (GMM, plot_error_ellipses, kmeansplusplus_initialization,
+                  covariance_initialization)
 
-    __all__.extend(["MVN", "plot_error_ellipse", "GMM", "plot_error_ellipses",
-                    "kmeansplusplus_initialization", "covariance_initialization"])
+__all__.extend(["MVN", "plot_error_ellipse", "GMM", "plot_error_ellipses",
+                "kmeansplusplus_initialization", "covariance_initialization"])
